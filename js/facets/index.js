eff54857eb985e2e209efeb782eabc2739b6b5db
Arjun Poudel <arjunpoudel703@gmail.com>
2024-03-28 18:00:31 +0545

    ...

--- js/facets/index.js
+++ js/facets/index.js
@@ -4,48 +4,46 @@ function generateFacetContent(data){
 	return data.facets.map(f => {
 	    return`
 			<div class="facet facet-${f.value}">
-			<div class="facet-header">
-				<h5>${f.label}</h5>
-			</div>
-			<div class="facet-options">
-				<div class="facet-options-inner">
-					${
-						f.options.map(o => {
-						    const inputID = generateRandomString() 
-						    return`
-								<div class="facet-option">
-									<div class="facet-input-and-label">
-									  	<div class="facet-input">
-										    <input type="checkbox"
-										    	id="cb-${inputID}"
-												onChange="handleNonTreeFacetOptionChange(event)"
-												value="${o.value}"  
-												data-facet="${f.value}"
-												data-value="${o.value}"
-										    />
-									  	</div>
-										<div class="facet-label-wrapper">
-											<label class="facet-label" for="cb-${inputID}">
-										    	${o.label}
-											</label>
+				<div class="facet-header">
+					<h5>${f.label}</h5>
+				</div>
+				<div class="facet-options">
+					<div class="facet-options-inner">
+						${
+							f.options.map(o => {
+							    const inputID = generateRandomString() 
+							    return`
+									<div class="facet-option">
+										<div class="facet-input-and-label">
+										  	<div class="facet-input">
+											    <input type="checkbox"
+											    	id="cbb-${inputID}"
+													onChange="handleNonTreeFacetOptionChange(event)"
+													value="${o.value}"  
+													data-facet="${f.value}"
+													data-value="${o.value}"
+											    />
+										  	</div>
+											<div class="facet-label-wrapper">
+												<label class="facet-label" for="cbb-${inputID}">
+											    	${o.label}
+												</label>
+											</div>
+										</div>
+										<div class="facet-hit-count">
+										 	${o.hits}
 										</div>
 									</div>
-									<div class="facet-hit-count">
-									 	${o.hits}
-									</div>
-								</div>
-						    `
-					  	}).join('')
-					}
+							    `
+						  	}).join('')
+						}
+					</div>
 				</div>
-			</div>
 			</div>
 	    `
   	}).join('')
 }
 
 function displayFacetContent(data){
-
 	document.querySelector('.facets').innerHTML = generateFacetContent(data);
-
 }
--- js/utils/index.js
+++ js/utils/index.js
@@ -1,4 +1,4 @@
-function generateRandomString(length) {
+function generateRandomString(length = 10) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
 
